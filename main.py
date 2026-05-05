8a612a1890531023ff48d1e01f2d73be34404c80
safwanahendouz <safwanahendouz241@gmail.com>
2026-05-05 22:04:08 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from tkinter import *
-from tkinter import tkk,messagebox
-import googletrans
-import textblob
+# from tkinter import tkk,messagebox
+# import googletrans
+# import textblob
 
 root=Tk()
 root.title("Google Translator")
 root.geometry("1080x400")
 root.configure(bg="white")
-root.mainloop
+root.mainloop()
 
 #icon
 
