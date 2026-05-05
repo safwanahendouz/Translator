91088cae83318fbc00589e5ec53fa64dd962e83b
safwanahendouz <safwanahendouz241@gmail.com>
2026-05-05 22:25:59 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -6,8 +6,21 @@ from tkinter import *
 root=Tk()
 root.title("Google Translator")
 root.geometry("1080x400")
+
+#icon
+image_icon=PhotoImage(file="google.png")
+root.iconphoto(False,image_icon)
+
+language=googletrans.LANGUAGES
+languageV=list(language.values())
+lang1=language.keys()
+
+combo1=ttk.Combobox(root,values=languageV,font="Roboto 14",state="r")
+combo1.place(x=110,y=20)
+combo1.set("ENGLISH")
+
 root.configure(bg="white")
 root.mainloop()
 
-#icon
+
 
